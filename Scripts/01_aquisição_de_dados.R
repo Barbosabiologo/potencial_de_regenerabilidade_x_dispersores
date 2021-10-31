edafe4f35515c3fa22a3ccab68d5086239d1becc
SetsuoTahara <setsuotahara@gmail.com>
2021-10-31 13:53:51 -0300

    adicao script de teste

---
+++ Scripts/01_aquisição_de_dados.R
@@ -0,0 +1,5 @@
+#### Potencial de regenerabilidade de acordo com a presença de dispersores: morcegos, primatas, roedores e aves###
+#### SCRIPT PARA ORGANIZAÇÃO DOS DADOS QUE SERÃO USADOS NO PROJETO####
+## Gupo guabiroba##
+# Criação 31/10/2021
+
